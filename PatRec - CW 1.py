be24d0e2b20a4dc51316c27e51d812dc5686a38e
gg2316 <33486356+gg2316@users.noreply.github.com>
2019-11-04 20:21:12 +0000

    Test commit

--- PatRec - CW 1.py
+++ PatRec - CW 1.py
@@ -10,3 +10,5 @@ print(mat_content)
 # Array X contains the face data, each column is one image from a total of 520 images.
 # Array l contains the label (face identity) for each image.
 
+x = 3
+print(x)
